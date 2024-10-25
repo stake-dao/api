53a2d46c40cb6aa92a739c0cb6a5bcb40b1d8b1e
Laouni <99427751+Lao0ni@users.noreply.github.com>
2024-10-25 13:27:12 +0200

    :green_heart: overwrite if present

--- automation/get_active_users_proofs.mk
+++ automation/get_active_users_proofs.mk
@@ -50,8 +50,9 @@ run-vm-active-proofs: get-current-epoch run-vm-all-platforms
 move-files:
 	@echo "Moving files..."
 	mkdir -p api/votemarket/$(CURRENT_EPOCH)/
-	mv temp/votemarket-proofs-script/temp/* api/votemarket/$(CURRENT_EPOCH)/
-	rm api/votemarket/$(CURRENT_EPOCH)/all_platforms.json
+	cp -Rf temp/votemarket-proofs-script/temp/* api/votemarket/$(CURRENT_EPOCH)/
+	rm -rf temp/votemarket-proofs-script/temp/*
+	@echo "Files moved successfully"
 
 .PHONY: clean
 clean:
