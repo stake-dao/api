7df5b8eb8f9243ba987e9f30b1bc7c8e9d6fcff0
0xVe <0xve@protonmail.com>
2024-11-20 22:22:48 +0100

    chore: 🔧 update bsc rpc

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const RPC = {
   1: 'https://ethereum-rpc.publicnode.com',
-  56: 'https://rpc.ankr.com/bsc',
+  56: 'https://bsc-rpc.publicnode.com',
+  // 56: 'https://rpc.ankr.com/bsc',
   137: 'https://polygon-bor-rpc.publicnode.com',
   252: 'https://rpc.frax.com',
   // 252: 'https://fraxtal.drpc.org',
