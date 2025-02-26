40ae4cc0e1eac000a08f8ba8cd2086c6a55b8192
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2025-02-26 14:23:43 +0100

    Add parquetjs and limit Votium merkles processing to delegators

--- scripts/votemarket/updateVotiumMerkles.ts
+++ scripts/votemarket/updateVotiumMerkles.ts
@@ -12,6 +12,32 @@ const updateVotiumMerkles = async () => {
       error: '❌ - An error occured during the Votium merkles update.',
     },
   })
+
+  {/* // Parquet storage
+  const schema = new parquet.ParquetSchema({
+    user: { type: 'UTF8' },
+    token: { type: 'UTF8' },
+    index: { type: 'INT32' },
+    amount: { type: 'INT64' },
+    proof: { type: 'UTF8', repeated: true }
+  });
+
+  const writer = await parquet.ParquetWriter.openFile(schema, 'api/votemarket/votium_merkles.parket');
+
+        for (const [user, userData] of Object.entries(votiumMerkles.claims)) {
+          for (const [token, data] of Object.entries((userData as any).tokens)) {
+              await writer.appendRow({
+                  user: user,
+                  token: token,
+                  index: (data as any).index,
+                  amount: (data as any).amount,
+                  proof: (data as any).proof
+              });
+          }
+      }
+    await writer.close();  
+    
+  */}
 }
 
 updateVotiumMerkles()
