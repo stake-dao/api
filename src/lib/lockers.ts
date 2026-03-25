1d427433549aedc94379c58f58d56b74fd3ef4ef
0xVe <0xve@protonmail.com>
2026-03-25 14:42:05 +0100

    feat: use hub api for prices

--- src/lib/lockers.ts
+++ src/lib/lockers.ts
@@ -1,8 +1,7 @@
 import { fetchLockers } from '@stake-dao/reader'
 import memoize from 'memoizee'
 import { base, bsc, fraxtal, linea, mainnet } from 'viem/chains'
-import { MEMO_MAX_AGE, getPrices, publicClient } from './utils'
-import { tokens } from '@stake-dao/constants'
+import { MEMO_MAX_AGE } from './utils'
 import { createPublicClient, http } from 'viem'
 import { RPC } from './constants'
 
@@ -10,17 +9,10 @@ require('dotenv').config()
 
 export const getLockers = memoize(
   async () => {
-    const pricesPromises = await Promise.all(
-      [mainnet.id, bsc.id, base.id, linea.id, fraxtal.id].map((chainId) =>
-        getPrices(
-          tokens.filter((t) => t.chainId === chainId),
-          chainId,
-        ),
-      ),
-    )
+    const prices = await fetch("https://data-hub.contact-69d.workers.dev/v1/prices").then(res => res.json())
 
     const lockers = await fetchLockers({
-      prices: pricesPromises.flat(),
+      prices,
       provider: {
         [mainnet.id]: createPublicClient({ chain: mainnet, transport: http(RPC[mainnet.id]) }),
         [bsc.id]: createPublicClient({ chain: bsc, transport: http(RPC[bsc.id]) }),
